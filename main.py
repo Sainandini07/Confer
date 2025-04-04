7c1f542548ab6baf3fad638d4509a68a72a04ad5
Sainandini07 <suvarchala.sai.nandini.peesapati@Vanderbilt.Edu>
2025-04-04 11:43:39 -0500

    initial commits of the main.py file.

---
+++ main.py
@@ -0,0 +1,17 @@
+class whole():
+    def __init__(self):
+
+        return
+        
+    def llm(self, prompt, content):
+        '''This method takes a prompt and content and returns a response object.'''
+        return
+    
+    def pdfParser(self, pdf):
+        '''This method takes in the pdf and returns its content in sections as an object.'''
+        return
+    
+    #def addParser(this links the both functions/methods)
+
+
+# helper functions
